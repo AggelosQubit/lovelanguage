501fe86b159cde61f22b9a06a6befdf8490a5694
AggelosQubit <aggelosqubit@gmail.com>
2023-04-12 16:39:55 +0200

    Router Add Functionnal, NavBar Updated UI OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
 import LoveForm	from './components/Loveform';
 import './App.css';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 
 function App(){
 
 	return (
-		<div className="container">
-			<LoveForm/>
-		</div>
+		<Router>
+			<div className="App container">
+				<Routes>
+					<Route path='/' />
+
+
+					<Route path='/' />
+					<Route path='/' />
+					<Route path='/' />
+					<Route path='/' />
+					<Route path='/' />
+
+					<Route path='/KnowYourLL' element={<LoveForm/>} />
+
+					<Route path='/' />
+
+				</Routes>
+			</div>
+		</Router>
+
+
 	);
 }
 
--- src/components/NavBarComponent/NavBarComponent.tsx
+++ src/components/NavBarComponent/NavBarComponent.tsx
@@ -4,15 +4,14 @@ import { Navbar,Nav,NavDropdown,Form,FormControl,Button } from "react-bootstrap"
 function NavBarComponent(){
 
     return(
-        <Navbar bg="dark" expand="lg" variant="dark">
-            <Navbar.Brand href="#">
+        <Navbar bg="dark" expand="lg">
+            <Navbar.Brand href="/">
             <img className="img-thumbnail" width={50} height={50} src="/img/LLL1.png" />
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <NavDropdown title="Langages ❤️" className='titleNav'  id="basic-nav-dropdown">
-                        
+                    <NavDropdown title="Langages ❤️" className='titleNav'  id="titleBehavior">
                         <NavDropdown.Item href="#">Les mots valorisants</NavDropdown.Item>
                         <NavDropdown.Divider />
                         <NavDropdown.Item href="#">Les moments de qualité</NavDropdown.Item>
@@ -24,7 +23,7 @@ function NavBarComponent(){
                         <NavDropdown.Item href="#">Le toucher physique </NavDropdown.Item>
                     </NavDropdown>
 
-                    <Nav.Link href="#">Connaître Votre Langage 💻</Nav.Link>
+                    <Nav.Link id="titleBehavior" className='titleNav'  href="/KnowYourLL">Connaître Votre Langage 💻</Nav.Link>
                 </Nav>
                 <Form className="offset-md-7  col-md-2 d-flex">
                     <FormControl type="search" placeholder="Search" className="ml-2" aria-label="Search"/>
