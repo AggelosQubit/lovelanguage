564c002e3185c2bac7e377c12fce87e72c159d96
AggelosQubit <aggelosqubit@gmail.com>
2023-08-06 22:13:52 +0200

    Handling Mobile Device view for NavBar Ok Search Element inline KO

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import LoveForm	from './components/LoveformComponent/Loveform';
+import LoveForm from './components/LoveformComponent/Loveform';
 import './App.css';
-import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBarComponent from './components/NavBarComponent/NavBarComponent';
 import FooterComponent from './components/FooterComponent/FooterComponent';
 
-function App(){
+function App() {
 
 	return (
 
 		<Router>
 			<div className="App">
-				<NavBarComponent/>
+				<NavBarComponent />
 				<Routes>
-					<Route path='/' /> 
+					<Route path='/KnowYourLL' element={<LoveForm />} />
 					<Route path='/' />
-					<Route path='/KnowYourLL' element={<LoveForm/>} />
-
-					<Route path='/' />
-
 				</Routes>
-				<FooterComponent/>
+				<FooterComponent />
 			</div>
 		</Router>
 	);
--- src/components/NavBarComponent/NavBarComponent.tsx
+++ src/components/NavBarComponent/NavBarComponent.tsx
@@ -3,13 +3,13 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-boots
 
 function NavBarComponent() {
     return (
-        <div className='generalComponent'>
+        <div className="generalComponent" >
             <div>
                 <a href="/">
                     <img className="img-thumbnail" width={50} height={50} src="/img/LLL1.png" />
                 </a>
             </div>
-            <div>
+            <div className="d-none d-md-block">
                 <NavDropdown title="Langages ❤️" id="titleBehavior">
                     <NavDropdown.Item href="#">Les mots valorisants     </NavDropdown.Item><NavDropdown.Divider />
                     <NavDropdown.Item href="#">Les moments de qualité   </NavDropdown.Item><NavDropdown.Divider />
@@ -19,12 +19,25 @@ function NavBarComponent() {
                 </NavDropdown>
             </div>
 
-            <div >
+            <div className="d-none d-md-block" >
                 <a href="/KnowYourLL" id="titleBehavior"> Connaître Votre Langage 💻</a>
             </div>
-            <div className="searchElement">
-                <FormControl type="search" placeholder="Search" className="ml-2" aria-label="Search" />
+            <div className="d-none d-md-block searchElement">
+                <FormControl type="search" placeholder="Search" aria-label="Search" />
                 <Button variant="outline-success">Search</Button>
+            </div>
+            <div className="d-sm-none">
+                <NavDropdown align="end" className='centerIt' title="Menu" id="titleBehavior">
+                    <NavDropdown className="paddingItem" title=" Langages ❤️">
+                        <NavDropdown.Item href="#">Les mots valorisants     </NavDropdown.Item><NavDropdown.Divider />
+                        <NavDropdown.Item href="#">Les moments de qualité   </NavDropdown.Item><NavDropdown.Divider />
+                        <NavDropdown.Item href="#">Les cadeaux              </NavDropdown.Item><NavDropdown.Divider />
+                        <NavDropdown.Item href="#">Les services rendus      </NavDropdown.Item><NavDropdown.Divider />
+                        <NavDropdown.Item href="#">Le toucher physique      </NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="/KnowYourLL"> Connaître Votre Langage 💻     </NavDropdown.Item>
+                </NavDropdown>
             </div>
         </div>
     );
