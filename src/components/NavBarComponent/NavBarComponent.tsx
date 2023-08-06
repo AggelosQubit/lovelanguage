f83d64bbeebe1f5b0bbe464ed326e7b1e431fac1
AggelosQubit <aggelosqubit@gmail.com>
2023-08-06 20:57:51 +0200

    Navbar Version 1 :  Spacing OK

--- src/components/NavBarComponent/NavBarComponent.tsx
+++ src/components/NavBarComponent/NavBarComponent.tsx
@@ -3,19 +3,22 @@ import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-boots
 
 function NavBarComponent() {
     return (
-        <Navbar className='generalComponent' bg="dark" expand="lg" style={{ border: "solid 1px yellow" }}>
-            <Navbar.Brand href="/"><img className="img-thumbnail" width={50} height={50} src="/img/LLL1.png" /></Navbar.Brand>
-            <Navbar.Collapse id="basic-navbar-nav" className='langagesElement'>
-                <Nav className="">
-                    <NavDropdown title="Langages ❤️" id="titleBehavior">
-                        <NavDropdown.Item href="#">Les mots valorisants</NavDropdown.Item>  <NavDropdown.Divider />
-                        <NavDropdown.Item href="#">Les moments de qualité</NavDropdown.Item><NavDropdown.Divider />
-                        <NavDropdown.Item href="#">Les cadeaux</NavDropdown.Item>           <NavDropdown.Divider />
-                        <NavDropdown.Item href="#">Les services rendus </NavDropdown.Item>  <NavDropdown.Divider />
-                        <NavDropdown.Item href="#">Le toucher physique </NavDropdown.Item>
-                    </NavDropdown>
-                </Nav>
-            </Navbar.Collapse>
+        <div className='generalComponent'>
+            <div>
+                <a href="/">
+                    <img className="img-thumbnail" width={50} height={50} src="/img/LLL1.png" />
+                </a>
+            </div>
+            <div>
+                <NavDropdown title="Langages ❤️" id="titleBehavior">
+                    <NavDropdown.Item href="#">Les mots valorisants     </NavDropdown.Item><NavDropdown.Divider />
+                    <NavDropdown.Item href="#">Les moments de qualité   </NavDropdown.Item><NavDropdown.Divider />
+                    <NavDropdown.Item href="#">Les cadeaux              </NavDropdown.Item><NavDropdown.Divider />
+                    <NavDropdown.Item href="#">Les services rendus      </NavDropdown.Item><NavDropdown.Divider />
+                    <NavDropdown.Item href="#">Le toucher physique      </NavDropdown.Item>
+                </NavDropdown>
+            </div>
+
             <div >
                 <a href="/KnowYourLL" id="titleBehavior"> Connaître Votre Langage 💻</a>
             </div>
@@ -23,7 +26,7 @@ function NavBarComponent() {
                 <FormControl type="search" placeholder="Search" className="ml-2" aria-label="Search" />
                 <Button variant="outline-success">Search</Button>
             </div>
-        </Navbar>
+        </div>
     );
 }
 
